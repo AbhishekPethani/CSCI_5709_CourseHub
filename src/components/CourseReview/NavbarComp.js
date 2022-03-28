8338c448788cad4bf8b448ce45d410d14e09c150
Ridampreet Jaggi <ridam.preet@gmail.com>
2022-03-28 02:34:50 -0300

    discounts added

--- src/components/CourseReview/NavbarComp.js
+++ src/components/CourseReview/NavbarComp.js
@@ -1,51 +1,81 @@
 /*=======================================================
  Author: [Abhishek Pareshbhai Pethani] (ab823206@dal.ca)
 ========================================================= */
-import React from 'react';
-import {Navbar, Container, Nav} from 'react-bootstrap';
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import CoursePage from '../../pages/CoursePage';
-import Courses from '../../pages/Courses';
-import Discount from '../../pages/Discount';
-import Home from '../../pages/Home';
-import MyAccount from '../../pages/MyAccount';
-import MyCourses from '../../pages/MyCourses';
-import Order from '../../pages/Order';
+import CoursePage from "../../pages/CoursePage";
+import Courses from "../../pages/Courses";
+import Discount from "../../pages/Discount";
+import Home from "../../pages/Home";
+import MyAccount from "../../pages/MyAccount";
+import MyCourses from "../../pages/MyCourses";
+import Coupons from "../coupons";
+import Order from "../../pages/Order";
 
 const NavbarComp = () => {
   return (
     <Router>
-    <div>
-      <Navbar style={{backgroundColor:"#3f51b5"}} variant={"dark"} expand="lg" >
-        <Container fluid>
-          <Navbar.Brand  as={Link} to={"/home"}>CourseHub</Navbar.Brand>          
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" >
-            <Nav className="me-auto">
-              <Nav.Link style={{color:"white"}} as={Link} to={"/home"}> Home </Nav.Link>
-              <Nav.Link style={{color:"white"}} as={Link} to={"/my-courses"}> My Courses </Nav.Link>
-              <Nav.Link style={{color:"white"}} as={Link} to={"/my-account"}> My Account </Nav.Link>
-              <Nav.Link style={{color:"white"}} as={Link} to={"/discount"}> Discounts </Nav.Link>
-              <Nav.Link style={{color:"white"}} as={Link} to={"/order"}> My Orders </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
-    <div>
-      <Routes>
-        <Route exact path="/" element={<Home/>}  />
-        <Route exact path="/home" element={<Home/>}  />
-        <Route exact path="/courses" element={<Courses/>}  />
-        <Route exact path="/my-courses" element={<MyCourses/>}  />
-        <Route exact path="/my-account" element={<MyAccount/>}  />
-        <Route exact path="/discount" element={<Discount/>}  />
-        <Route exact path="/order" element={<Order />}  />
-        <Route exact path="/courses/:courseName" element={<CoursePage />}  />
-      </Routes>
-    </div>
-  </Router>
-  )
-}
+      <div>
+        <Navbar
+          style={{ backgroundColor: "#3f51b5" }}
+          variant={"dark"}
+          expand="lg"
+        >
+          <Container fluid>
+            <Navbar.Brand as={Link} to={"/home"}>
+              CourseHub
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link style={{ color: "white" }} as={Link} to={"/home"}>
+                  {" "}
+                  Home{" "}
+                </Nav.Link>
+                <Nav.Link
+                  style={{ color: "white" }}
+                  as={Link}
+                  to={"/my-courses"}
+                >
+                  {" "}
+                  My Courses{" "}
+                </Nav.Link>
+                <Nav.Link
+                  style={{ color: "white" }}
+                  as={Link}
+                  to={"/my-account"}
+                >
+                  {" "}
+                  My Account{" "}
+                </Nav.Link>
+                <Nav.Link style={{ color: "white" }} as={Link} to={"/discount"}>
+                  {" "}
+                  Discounts{" "}
+                </Nav.Link>
+                <Nav.Link style={{ color: "white" }} as={Link} to={"/order"}>
+                  {" "}
+                  My Orders{" "}
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/courses" element={<Courses />} />
+          <Route exact path="/my-courses" element={<MyCourses />} />
+          <Route exact path="/my-account" element={<MyAccount />} />
+          <Route exact path="/discount" element={<Coupons />} />
+          <Route exact path="/order" element={<Order />} />
+          <Route exact path="/courses/:courseName" element={<CoursePage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-export default NavbarComp
+export default NavbarComp;
---
+++ src/components/coupons.js
@@ -0,0 +1,101 @@
+import { ButtonBase, Card, CardActions, CardContent } from "@mui/material";
+import React, { useEffect, useState } from "react";
+
+import { Button, CardActionArea, TextField } from "@mui/material";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@mui/material/Typography";
+
+import CardMedia from "@mui/material/CardMedia";
+import Appbar from "./Appbar";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+
+import { Navigate } from "react-router";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  background: {
+    height: "2000px",
+    background: "linear-gradient(#9198e5,#e66465)"
+  }
+}));
+export default function Profile() {
+  const [arrOfItems, setarrOffItems] = useState([]);
+  const [searchInp, setsearchInp] = useState("");
+
+  function handleChange(event) {
+    event.preventDefault();
+    setsearchInp(event.target.value);
+  }
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    fetch("https://tutorial5709-3.herokuapp.com/coupons/coupons", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        setarrOffItems(data.users);
+      });
+  }, []);
+  const handleClick = event => {
+    event.preventDefault();
+    alert("card clciked");
+  };
+
+  const classes = useStyles();
+  return (
+    <div className={classes.background}>
+      {/* <Appbar></Appbar> */}
+
+      <div className={classes.root}>
+        <Grid container spacing={2}>
+          {arrOfItems
+            .filter(user => {
+              return user;
+            })
+            .map(user => (
+              <Grid item xs={3}>
+                <Card
+                  sx={{
+                    margin: "30px",
+                    background: "#E1E7EB"
+                  }}
+                  variant="outlined"
+                >
+                  <CardMedia
+                    component="img"
+                    height="265"
+                    image={user.src}
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {user.value} % OFF
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Apply this coupon code on your checkout to avail a dicount
+                      of {user.value}% on your total order value. T&C apply
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Typography variant="body2" color="text.primary">
+                      Coupon Code: {user.couponCode}
+                    </Typography>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+        </Grid>
+      </div>
+    </div>
+  );
+}
+
+// image citations:https://www.dreamstime.com/stock-illustration-off-red-rubber-stamp-over-white-background-image88004210
